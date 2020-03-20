807fb63ba33257476093779398bae0014c23b75c
Noam Sauer-Utley <noamsauerutley@gmail.com>
2020-03-20 13:21:34 -0400

    Add additional test case conditionals & tests)

--- greatestCommonDivisor.js
+++ greatestCommonDivisor.js
@@ -1,10 +1,22 @@
 let greatestCommonDivisor = (firstNum, secondNum) => {  
-    // this base case stops the recursion once secondNum hits zero (falsey)
-    if (!secondNum) {  
-        return firstNum;  
-    }  
-    // recursively call greatestCommonDivisor function with params secondnum & firstNum modulo secondNum
-    return greatestCommonDivisor(secondNum, firstNum % secondNum);  
- };  
+    if (firstNum < 0){
+        firstNum = -firstNum
+    }
+
+    if (secondNum < 0){
+        secondNum = -secondNum
+    }
+
+    if (Number.isInteger(firstNum) && Number.isInteger(secondNum) && !(firstNum===0 && secondNum===0)){
+
+        // this base case stops the recursion once secondNum hits zero (falsey)
+        if (!secondNum) {  
+            return firstNum 
+        }  
+        // recursively call greatestCommonDivisor function with params secondnum & firstNum modulo secondNum
+        return greatestCommonDivisor(secondNum, firstNum % secondNum)
+    }
+    return "Please enter two integers which are not both zero"  
+ }  
 
  module.exports = greatestCommonDivisor
--- test/greatestCommonDivisor.spec.js
+++ test/greatestCommonDivisor.spec.js
@@ -3,8 +3,24 @@ let greatestCommonDivisor = require('../greatestCommonDivisor')
 describe('#greatestCommonDivisor', () => {
     test("if one variable is zero, the other variable is returned", () => {
         expect(greatestCommonDivisor(0, 5)).toEqual(5)
+        expect(greatestCommonDivisor(5, 0)).toEqual(5)
     })
     test("it returns the greatest common divisor", () => {
         expect(greatestCommonDivisor(8, 12)).toEqual(4)
     })
+    test("it returns the greatest common divisor when one or both number(s) is negative", () => {
+        expect(greatestCommonDivisor(-8, 12)).toEqual(4)
+        expect(greatestCommonDivisor(-8, -12)).toEqual(4)
+        expect(greatestCommonDivisor(8, -12)).toEqual(4)
+    })
+    test("it returns an error message if given incorrect params", () => {
+        expect(greatestCommonDivisor()).toEqual("Please enter two integers which are not both zero")
+        expect(greatestCommonDivisor(5)).toEqual("Please enter two integers which are not both zero")
+        expect(greatestCommonDivisor(0, 0)).toEqual("Please enter two integers which are not both zero")
+        expect(greatestCommonDivisor("hello", 5)).toEqual("Please enter two integers which are not both zero")
+        expect(greatestCommonDivisor(5, true)).toEqual("Please enter two integers which are not both zero")
+        expect(greatestCommonDivisor(null, 5)).toEqual("Please enter two integers which are not both zero")
+        expect(greatestCommonDivisor(undefined, 5)).toEqual("Please enter two integers which are not both zero")
+    })
+    
 })
